689a01caf8f545633ea0ae0a8356fdc833518204
Dilavar Afreed <dilavar4598@gmail.com>
2026-02-25 00:53:51 +0530

    v1.0: implement JWT authentication with login endpoint

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -4,6 +4,8 @@ from app.db.session import SessionLocal
 from app.models.user import User
 from app.schemas.user import UserCreate, UserResponse
 from app.core.security import hash_password
+from app.schemas.user import UserLogin, Token
+from app.core.security import verify_password, create_access_token
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
@@ -35,3 +37,19 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return new_user
+
+@router.post("/login", response_model=Token)
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user:
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+
+    if not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+
+    access_token = create_access_token(
+        data={"sub": str(db_user.id), "email": db_user.email}
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
