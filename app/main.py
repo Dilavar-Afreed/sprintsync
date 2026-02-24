e64a4841a7e48eb7afba13f4d9b1ebedfaafbb99
Dilavar Afreed <dilavar4598@gmail.com>
2026-02-24 23:25:44 +0530

    v0.5: connect FastAPI to PostgreSQL using SQLAlchemy

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from app.db.session import engine
+from sqlalchemy import text
 
 app = FastAPI(
     title="SprintSync API",
     version="0.1.0",
     description="Internal sprint tracking tool for AI consultancy"
 )
+
+
+@app.on_event("startup")
+def test_db_connection():
+    with engine.connect() as connection:
+        connection.execute(text("SELECT 1"))
+    print("Database connected successfully ✅")
+
 
 @app.get("/health")
 def health_check():
